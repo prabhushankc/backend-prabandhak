239b508fc850ab6fae1f4eb2816be2a5d454ca79
sunilgrg10 <sunilgrg352@gmail.com>
2022-05-20 07:06:40 +0545

    delete-account

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { signin, signup, singleUser, updateSingleUser } from '../controller/user.js';
+import { signin, signup, singleUser, updateSingleUser, deleteUser } from '../controller/user.js';
 import auth from '../middleware/auth.js';
 const router = express.Router();
 
-router.post('/signin', signin);
-router.post('/signup', signup);
-router.get('/singleuser/:id', auth, singleUser);
-router.patch('/updatesingleuser/:id', auth, updateSingleUser);
+router.post('/signin', signin)
+router.post('/signup', signup)
+router.get('/singleuser/:id', auth, singleUser)
+router.patch('/updatesingleuser/:id', auth, updateSingleUser)
+router.delete('/deleteuser/:id', auth, deleteUser)
 export default router;
