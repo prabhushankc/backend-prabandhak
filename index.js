2e2c50e33d34ae243267b15b07ab8b071643e2ee
AnupLamsal <anuplamsal7410@gmail.com>
2022-05-27 17:53:00 +0545

    Room Backend Setup

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
-
-
-import userRoutes from "./routes/users.js";
 import roomRoutes from "./routes/roomRoute.js";
 
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import userRoutes from './routes/users.js'
-import homePageRoutes from './routes/homePage.js'
+import userRoutes from "./routes/users.js";
+import homePageRoutes from "./routes/homePage.js";
 
 const app = express();
 
@@ -21,6 +18,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/user", userRoutes);
+app.use("/homepage", homePageRoutes);
 app.use("/api/rooms", roomRoutes);
 
 // Using morgan for dev dependancy
@@ -31,26 +29,26 @@ if (process.env.NODE_ENV === "development") {
 // Setting up own error middleware
 app.use(notFound);
 app.use(errorHandler);
-app.use('/user', userRoutes)
-app.use('/homePage', homePageRoutes)
-app.get('/', (req, res) => {
-  res.send('Hello this is HMS')
-})
+app.use("/user", userRoutes);
+app.use("/homePage", homePageRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello this is HMS");
+});
 
 const PORT = process.env.PORT || 5000;
 
-// mongoose
-//   .connect(process.env.CONNECTION_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => app.listen(PORT, console.log(`Server running ${PORT}`)))
-//   .catch(error => console.log(error));
-
 mongoose
-  .connect(process.env.Anup_Mongo_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => app.listen(PORT, console.log(`Server running ${PORT}`)))
   .catch(error => console.log(error));
+
+// mongoose
+//   .connect(process.env.Anup_Mongo_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => app.listen(PORT, console.log(`Server running ${PORT}`)))
+//   .catch(error => console.log(error));
--- routes/homePage.js
+++ routes/homePage.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getHomePage, createHomePage } from "../controller/homePage.js";
-import auth from "../middleware/auth.js";
+import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
 router.get("/", getHomePage);
 router.post("/", auth, createHomePage);
+
 export default router;
