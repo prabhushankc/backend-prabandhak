aa3a77825e9bdecb2ec5a99c8f2ec0bb72a33fff
aayushsapkota1 <kodaayush@gmail.com>
2022-06-02 17:14:53 +0545

    Delete room api

--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { createRoom, getRooms } from "../controller/roomController.js";
+import {
+  createRoom,
+  getRooms,
+  deleteRoom,
+} from "../controller/roomController.js";
 import { auth } from "../middleware/auth.js";
 
 const router = Router();
 
 router.route("/").get(getRooms).post(auth, createRoom);
+router.route("/:id").delete(auth, deleteRoom);
 
 export default router;
