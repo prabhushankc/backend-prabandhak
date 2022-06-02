b424f85e7fb9053ae506cd5b02df8ee1511ca740
AnupLamsal <anuplamsal7410@gmail.com>
2022-06-02 19:18:53 +0545

    Update room API setup

--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { createRoom, getRooms } from "../controller/roomController.js";
+import {
+  createRoom,
+  getRoomById,
+  getRooms,
+  updateRoom,
+} from "../controller/roomController.js";
 import { auth } from "../middleware/auth.js";
 
 const router = Router();
 
 router.route("/").get(getRooms).post(auth, createRoom);
+router.route("/:id").put(auth, updateRoom).get(getRoomById);
 
 export default router;
