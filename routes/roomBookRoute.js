441dc8692dfa60c5d21f533a88d6157d656ed6c6
AnupLamsal <anuplamsal7410@gmail.com>
2022-06-16 18:39:02 +0545

    Sorting and Searching of hotel rooms

--- routes/roomBookRoute.js
+++ routes/roomBookRoute.js
@@ -4,9 +4,11 @@ import { auth, isAdmin } from "../middleware/auth.js";
 import {
   addBookRooms,
   getBookedRooms,
+  getMyBookedRooms,
 } from "../controller/roomBookController.js";
 
 router.route("/:id").post(auth, addBookRooms);
 router.route("/").get(auth, isAdmin, getBookedRooms);
+router.route("/me").get(auth, getMyBookedRooms);
 
 export default router;
