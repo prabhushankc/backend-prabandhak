2eef02907962ef0209bcd67295598ec4e238f9c4
aayushsapkota1 <kodaayush@gmail.com>
2022-07-01 17:59:16 +0545

    Implementing comments for food

--- routes/foodPage.js
+++ routes/foodPage.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { getFoodPage, createFoodPage, deleteFood, updateFoodPage, getFoodBySearch } from "../controller/foodPage.js";
-import { checkAdmin } from "../middleware/auth.js";
+import { getFoodPage, createFoodPage, deleteFood, updateFoodPage, getFoodBySearch, getFoodById, createCommentFood, deleteCommentFood } from "../controller/foodPage.js";
+import { auth, checkAdmin } from "../middleware/auth.js";
 
 const router = express.Router();
 router.get("/", getFoodPage);
@@ -8,4 +8,7 @@ router.get("/search", getFoodBySearch);
 router.post("/", checkAdmin, createFoodPage);
 router.patch("/:id", checkAdmin, updateFoodPage);
 router.delete('/deletefood/:id', checkAdmin, deleteFood)
+router.patch("/comment/:id", auth, createCommentFood);
+router.delete("/comment/:id/:cmtuserId", auth, deleteCommentFood);
+router.get('/:id', getFoodById)
 export default router;
