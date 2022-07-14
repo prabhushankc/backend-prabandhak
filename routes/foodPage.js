10d4bbc414c2f0253710db659f6fd38b25401f46
aayushsapkota1 <kodaayush@gmail.com>
2022-07-14 17:37:00 +0545

    Report user foodpage backend

--- routes/foodPage.js
+++ routes/foodPage.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { getFoodPage, createFoodPage, deleteFood, updateFoodPage, getFoodBySearch, getFoodById, createCommentFood, deleteCommentFood } from "../controller/foodPage.js";
 import { auth, checkAdmin } from "../middleware/auth.js";
-
 const router = express.Router();
 router.get("/", getFoodPage);
 router.get("/search", getFoodBySearch);
